03d6bc8de5d5e54800a6bb771552a36d61286457
prnvjn <prnvjn@gmail.com>
2022-10-30 20:26:02 -0500

    2.14

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
@@ -15,7 +10,11 @@ const App = () => {
    setNewName(e.target.value)
 
   }
-
+useEffect(()=>{
+  axios
+    .get("http://localhost:3001/persons")
+    .then((res)=>setPersons(res.data))
+},[])
 
 const onAdd=(e)=>{
   e.preventDefault()
